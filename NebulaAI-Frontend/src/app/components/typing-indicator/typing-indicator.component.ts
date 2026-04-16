165640539aed63430a42dfdc35806f258b56c7f5
SanjanaPothireddy-AidenAi <sanjana.pothireddy@aidenai.com>
2026-04-17 00:16:26 +0530

    adding the angular frontend code base

---
+++ NebulaAI-Frontend/src/app/components/typing-indicator/typing-indicator.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typing-indicator',
+  standalone: true,
+  templateUrl: './typing-indicator.component.html',
+  styleUrls: ['./typing-indicator.component.css']
+})
+export class TypingIndicatorComponent {}
