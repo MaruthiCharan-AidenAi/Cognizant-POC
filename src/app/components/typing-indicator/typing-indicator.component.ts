bd07d510fef275a10d0e82b51cf333ad3f630cf1
SanjanaPothireddy-AidenAi <sanjana.pothireddy@aidenai.com>
2026-04-17 00:04:57 +0530

    Revert "Added analytics chatbot folder"
    This reverts commit cf8fc28a2bd634563bda2ae10de17d6d868577c7.

--- src/app/components/typing-indicator/typing-indicator.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-typing-indicator',
-  standalone: true,
-  templateUrl: './typing-indicator.component.html',
-  styleUrls: ['./typing-indicator.component.css']
-})
-export class TypingIndicatorComponent {}
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://lookerbackend-1007459863351.asia-south1.run.app/',
-  googleClientId: '1007459863351-7b5fds45vri4jj1aipsfhk4gnglp7629.apps.googleusercontent.com',
-};
